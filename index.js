cbfd9882c4e0498aeda53cc42410b962e9e3ebcc
Anton Molchanovskyi <mywear1@gmail.com>
2021-09-17 12:38:09 +0300

    procfile_add

--- index.js
+++ index.js
@@ -110,7 +110,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    // res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    const index = path.join(__dirname,"client", 'build', 'index.html');
+    res.sendFile(index);
   });
 }
 
