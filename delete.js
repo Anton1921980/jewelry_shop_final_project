4e714f2df1ca4a693785c96ede096bd389c918a8
Anton Molchanovskyi <mywear1@gmail.com>
2021-11-23 15:57:34 +0200

    5000_fix

--- delete.js
+++ delete.js
@@ -1,38 +1,44 @@
-var axios = require("axios");
+var axios = require( "axios" );
 
-function deleteAllProducts() {
-    axios
-      .post("http://localhost:5000/customers/login", {
-        loginOrEmail: "customer@gmail.com",
-        password: "1111111"
-      })
-      .then(response => {
-        let token = response.data.token;
-        console.log(token);
-        axios.get("http://localhost:5000/products/").then(products => {
+function deleteAllProducts ()
+{
+  axios
+    .post( "/customers/login", {
+      loginOrEmail: "customer@gmail.com",
+      password: "1111111"
+    } )
+    .then( response =>
+    {
+      let token = response.data.token;
+      console.log( token );
+      axios.get( "/products/" ).then( products =>
+      {
         //   console.log(products);
-          products.data
-            .forEach(element => {
-              let i = element.itemNo;            
-              //удаление по категориям
-              // if (element.categories === "rings"){
-              axios
-                .delete(`http://localhost:5000/products/${i}`, {
-                  headers: { Authorization: `${token}` }
-                })
-                .then(res => {
-                  console.log( "success",res);
-                  
-                })
-                .catch(err => {
-                  console.log('error',err);
-                });
-              // }
-            })            
-            // .catch(err => {
-            //   console.log(err);
-            // });
-        });
-      });
-  }
-  deleteAllProducts()
+        products.data
+          .forEach( element =>
+          {
+            let i = element.itemNo;
+            //удаление по категориям
+            // if (element.categories === "rings"){
+            axios
+              .delete( `/products/${ i }`, {
+                headers: { Authorization: `${ token }` }
+              } )
+              .then( res =>
+              {
+                console.log( "success", res );
+
+              } )
+              .catch( err =>
+              {
+                console.log( 'error', err );
+              } );
+            // }
+          } )
+        // .catch(err => {
+        //   console.log(err);
+        // });
+      } );
+    } );
+}
+deleteAllProducts()
